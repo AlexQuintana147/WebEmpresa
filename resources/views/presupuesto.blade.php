ff9fe4679c7c4e3db35c43f1c3f2d2f632fcc4dd
AlexQuintana147 <alexquintana12622@hotmail.com>
2025-02-25 16:35:11 -0500

    viewsNombrados

---
+++ resources/views/presupuesto.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>presupuesto</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.store('modal', {
+                open: false
+            })
+        })
+    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+<body class="bg-gray-100">
+    <div class="min-h-screen flex">
+        <!-- Sidebar -->
+        <x-sidebar />
+
+        <!-- Main Content -->
+        <div class="flex-1">
+            <!-- Header -->
+            <x-header />
+
+           
+        </div>
+    </div>
+</body>
+</html>
